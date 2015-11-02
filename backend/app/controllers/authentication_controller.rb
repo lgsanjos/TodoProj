7c975700dfb6d849f80a9aba0a730da9513d7122
Luiz Guilherme <lgsanjos@gmail.com>
2015-11-02 17:57:53 +0000

    Authentication login working

--- backend/app/controllers/authentication_controller.rb
+++ backend/app/controllers/authentication_controller.rb
@@ -1,10 +1,17 @@
 class AuthenticationController < ApplicationController
 
   def login
+    username = params[:username]
+    pass = params[:password]
 
-    user = { :id => 1, :username => 'name'}
+    user = User.where(:name => username, :password => pass).first
 
-   render json: user
+    if user.nil?
+      render :text => 'Failed to login', :status => :unauthorized
+    else
+      render json: user
+    end
+
   end
 
 end
--- backend/spec/controllers/authentication_controller_spec.rb
+++ backend/spec/controllers/authentication_controller_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
+require 'json'
 
 RSpec.describe AuthenticationController, type: :controller do
 
   it 'should return user id when login succeed' do
+    returned_user = User.new(:name => 'name', :password => 'pass123')
+    allow(User).to receive(:where).and_return([returned_user])
+
     post :login, { :username => 'name', :password => 'pass123' }, :type => :json
 
     expect(response.status).to be 200
-    expect(response.body).to be 200
+
+    parsed_body = JSON.parse response.body
+    expect(parsed_body['name']).to eql 'name'
+    expect(parsed_body['password']).to eql 'pass123'
+  end
+
+  it 'should return error message when login fail' do
+    post :login, { :username => 'user', :password => '23'}, :type => :json
+
+    expect(response.status).to be 401
+    expect(response.body).to eql 'Failed to login'
   end
 
 end
